19fb2955557850e866979a35f0e62a4ba59ed425
BornPosthumous <jeffrey.m.rivera@gmail.com>
2018-01-10 21:30:46 -0600

    Add pretty sliders, add checkboxes

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,13 @@
-//const styles = require('./index.scss')
 import * as React from 'react';
 import * as Rx    from 'rxjs';
 import CantorCanvas from "./cantorCanvas"
 import MeanderCanvas from "./meander"
+import Sidebar from 'react-sidebar'
+import Drawer from 'react-motion-drawer';
 
+const SideBar:any = Drawer;
+console.log("S", Sidebar);
 
-function createDefaultMeanderConfig(){
-  return {
-    sides        : 3,
-    flip         : true,
-    sideLength   : 400,
-    depth        : 4,
-    baseRotation : 0,
-    base : 4
-  }
-}
 type ConfigOption = {
   optionName : string,
   min        : number,
@@ -38,14 +31,15 @@ export default class App extends React.Component<any, any> {
 
   meanderConfig: Array<any> =
     [{ optionName: 'depth'       , min: 0 , max:7     , value: 3   },
-     { optionName: 'flip'        , min: 0 , max:1     , value: 0   , type: 'checkbox' },
      { optionName: 'sides'       , min: 2 , max:10    , value: 7   },
      { optionName: 'lineWidth'   , min: 0 , max:400   , value: 10  },
-     { optionName: 'noAnimation' , min: 1 , max:9     , value: 0   },
      { optionName: 'base'        , min: 2 , max:100   , value: 8   },
      { optionName: 'sideLength'  , min: 2 , max:800   , value: 300 },
      { optionName: 'baseRotation', min: 2 , max:800   , value: 0   },
-     { optionName: 'numSegments' , min: 2 , max:20000 , value: 300 }
+     { optionName: 'numSegments' , min: 2 , max:20000 , value: 300 },
+     { optionName: 'flip'        , min: 0 , max:1     , value: 0   , type: 'checkbox' },
+     { optionName: 'noAnimation' , min: 1 , max:9     , value: 0   , type: 'checkbox' },
+
     ]
 
   formInput$: Rx.BehaviorSubject<any> = new Rx.BehaviorSubject(null);
@@ -61,34 +55,46 @@ export default class App extends React.Component<any, any> {
     )
     .debounceTime(300)
     .distinctUntilChanged()
-    .map((x:any) => {
-      if(x && x.option && (x.value <= x.option.max )){
+      .map((x:any) => {
+        if(!x || !x.option) return;
+        if(x.option.max &&  x.value > x.option.max ) return;
+
         return this.meanderConfig.filter( option =>{
           return option.optionName == x.option.optionName;
         }).map( (y:any) => {
           y.value = x.value;
           let node = this.meanderConfig.reduce((acc:any, cur:any) =>{
-            acc[cur.optionName] = parseInt(cur.value)
+            if(cur && cur.type == 'checkbox' ){
+              console.log("cur", cur);
+              if(cur.value == 'on'  ){ cur.value = true; }
+              if(cur.value == 'off' ){ cur.value = false; }
+            }
+            if(typeof cur.value === 'string') cur.value = parseInt(cur.value)
+            acc[cur.optionName] = cur.value;
             return acc;
           }, {})
           //          new CantorCanvas( 'c', node)
-          node.noAnimation = false;
-          node.flip = false;
+          this.inputs = this.generateInputs(node);
           node.motifConfig = [0, Math.PI/2, -Math.PI/4, 0,
                               0, -Math.PI/3, -Math.PI/2, 0]
           console.log("Creating",node);
-          try{
-            new MeanderCanvas('c', node)
-          } catch(e){ console.log("X", e);}
-
-          });
-      }
+          new MeanderCanvas('c', node)
+        });
     })
 
   constructor(props:any){
     super(props)
     this.init()
 
+    this.state = {
+      sidebarOpen: false
+    }
+
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open: any) {
+    this.setState({sidebarOpen: open});
   }
 
   init(){
@@ -114,13 +120,13 @@ export default class App extends React.Component<any, any> {
 
     /* new CantorCanvas( 'c', node)*/
   }
-  generateInputs = () =>{
+  generateInputs = (node?:any) =>{
     return this.meanderConfig.map( ( option:any ,i:any ) => {
       return (
-          <div key={i}>
+          <div key={i} className="inputs">
           <label>{option.optionName}</label>
           <input id={option.optionName}
-                 type="range"
+                 type={option.type ? option.type : "range"}
                  min={option.min}
                  max={option.max}
                  onChange={(e:any) => this.handleChange({ option, value: e.target.value })}
@@ -129,10 +135,13 @@ export default class App extends React.Component<any, any> {
     })}
 
   render() {
+
+    var sidebarContent = <b>Sidebar content</b>;
+    const { openLeft, openRight } = this.state;
     return (
-      <div>
-        {this.inputs}
-      </div>
+        <div className="input-container">
+          {this.inputs}
+        </div>
     )
   }
 }
