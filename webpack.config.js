c82ff93eebeac99a642384f35da75e4575f8d5ae
BornPosthumous <jeffrey.m.rivera@gmail.com>
2021-09-01 18:50:42 -0400

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-var path    = require('path');
-var webpack = require('webpack');
+const path    = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: ['./src/index.tsx'],
   output: {
-    filename: 'bundle.js',
-    path: __dirname
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
+  // stats: 'minimal',
   devtool: 'eval-source-maps',
   module: {
     rules: [
@@ -17,7 +20,18 @@ module.exports = {
         }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html'
+    })
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
-  }
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 9000,
+    https: true,
+  },
 };
