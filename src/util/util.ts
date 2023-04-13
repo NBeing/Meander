7c5d8416b4afefd9112c3c41a07aeee59f5b564a
Nbeing <12501551+NBeing@users.noreply.github.com>
2023-04-13 11:29:01 -0400

    Upgrade TS and fix compilation errors

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,29 +1,32 @@
-import { merge } from 'rxjs/observable/merge';
+const windowWidth = () =>{
+  return window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+};
+const windowHeight = () =>{
+  return window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+}
+
+const getScreenDimensions = () => ({ width: windowWidth(), height: windowHeight()})
 
 export default {
-  windowWidth : () =>{
-    return window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
-  },
-  windowHeight : () =>{
-    return window.innerHeight
-      || document.documentElement.clientHeight
-      || document.body.clientHeight;
-  },
-  getScreenDimensions: () => ({ width: this.windowWidth, height: this.windowHeight}),
+  windowWidth ,
+  windowHeight,
+  getScreenDimensions
 }
 export const hasKey = key => obj => obj[key]
 
 export const parseFloatByKey = key => obj => Object.assign({}, obj, { [key] :parseFloat(obj[key])})
 
-export const parseMeanderConfigToMeanderCanvasOptions =
-  config => config.reduce(this.addOption, {})
-
 export const addOption = (acc:any, cur:any) => {
     acc[cur.optionName] = cur.value
     return acc
 }
+export const parseMeanderConfigToMeanderCanvasOptions =
+  config => config.reduce(addOption, {})
+
 export const createNodeFromStoreConfig = 
   state => Object.assign({}, 
                         parseMeanderConfigToMeanderCanvasOptions(state.canvas.config),
